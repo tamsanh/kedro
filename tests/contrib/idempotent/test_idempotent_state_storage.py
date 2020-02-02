f4d16760ddbd86476a767320fb10e2f01b02b0bd
Mengzhu <xiemengzhu0526@163.com>
2020-02-02 12:10:07 +0800

    Add test to IdempotentStateStorage

--- tests/contrib/idempotent/test_idempotent_state_storage.py
+++ tests/contrib/idempotent/test_idempotent_state_storage.py
@@ -1,77 +1,66 @@
 import pytest
-import random
-import os
+from uuid import uuid4
 
-from kedro.contrib.idempotent.idempotent_state_storage import IdempotentStateStorage, NODE_STATE_FILE_PATH
+from kedro.contrib.idempotent.idempotent_state_storage import IdempotentStateStorage
 
 
 @pytest.fixture
 def state_storage_no_update():
     # Pipeline: node3 -> node2 -> node1
     nodes = ['node1', 'node2', 'node3']
-    keys = [1, 2, 3]
-    return {
+
+    run_id_state = {
+        node: str(uuid4())
+        for index, node in enumerate(nodes)
+    }
+
+    input_state = {
         node: {
-            "key": keys[node_index],
-            "inputs": {
-                nodes[input_node_index]: keys[input_node_index]
-                for input_node_index in range(node_index)
-            }
+            nodes[input_node_index]: run_id_state[nodes[input_node_index]]
+            for input_node_index in range(node_index)
         }
         for node_index, node in enumerate(nodes)
+    }
+
+    return {
+        "run_id_state": run_id_state,
+        "input_state": input_state
     }
 
 
 @pytest.fixture
-def state_storage_with_update():
-    # Pipeline: node3 -> node2 -> node1
-    nodes = ['node1', 'node2', 'node3']
-    keys = [1, 2, 3]
-    return {
-        node: {
-            "key": random.choice(range(10, 20)),
-            "inputs": {
-                nodes[input_node_index]: keys[input_node_index]
-                for input_node_index in range(node_index)
-            }
-        }
-        for node_index, node in enumerate(nodes)
-    }
+def state_storage_with_update(state_storage_no_update):
+    state = state_storage_no_update.copy()
+
+    for node in state['run_id_state'].keys():
+        state['run_id_state'][node] = str(uuid4())
+
+    return state
 
 
 class TestIdempotentStateStorage:
 
     def test_node_inputs_have_not_changed(self, state_storage_no_update):
-        storage = IdempotentStateStorage(state_storage_no_update)
+        storage = IdempotentStateStorage(**state_storage_no_update)
         assert not storage.node_inputs_have_changed('node3', ['node2', 'node1'])
 
-    def test_node_inputs_have_changed(self, state_storage_with_update):
-        storage = IdempotentStateStorage(state_storage_with_update)
-        assert storage.node_inputs_have_changed('node3', ['node2', 'node1'])
+    def test_node_inputs_changed_with_input_removement(self, state_storage_no_update):
+        storage = IdempotentStateStorage(**state_storage_no_update)
         assert storage.node_inputs_have_changed('node3', ['node2'])
 
+    def test_node_inputs_run_id_changed(self, state_storage_with_update):
+        storage = IdempotentStateStorage(**state_storage_with_update)
+        assert storage.node_inputs_have_changed('node3', ['node2', 'node1'])
+
+    def test_update_run_id_for_node(self, state_storage_no_update):
+        new_run_id = str(uuid4())
+        storage = IdempotentStateStorage(**state_storage_no_update)
+        storage.update_run_id("node1", new_run_id)
+
+        assert storage.run_id_state['node1'] == new_run_id
+        assert storage.node_inputs_have_changed('node3', ['node2', 'node1'])
+
     def test_update_key(self, state_storage_with_update):
-        storage = IdempotentStateStorage(state_storage_with_update)
-        updated_state = storage.update_run_id('node3', ['node2', 'node1'])
-
-        assert updated_state['node3']['inputs']['node2'] == updated_state['node2']['key']
-        assert updated_state['node3']['inputs']['node1'] == updated_state['node1']['key']
-
-        updated_state = storage.update_run_id('node3', ['node2'])
-        assert updated_state['node3']['inputs']['node2'] == updated_state['node2']['key']
-        assert not updated_state['node3']['inputs'].get('node1', None)
-
-
-class TestIdempotentStateStorageIO:
-    def test_initial_state(self):
-        os.remove(NODE_STATE_FILE_PATH)
-        storage = IdempotentStateStorage()
-        assert storage.state == {}
-
-    def test_state_and_load(self):
-        initial_state = state_storage_no_update()
-        storage = IdempotentStateStorage(initial_state)
-        storage.save_state()
-
-        storage = IdempotentStateStorage()
-        assert storage.state == initial_state
+        storage = IdempotentStateStorage(**state_storage_with_update)
+        storage.update_inputs('node3', ['node2', 'node1'])
+        assert not storage.node_inputs_have_changed('node3', ['node2', 'node1'])
